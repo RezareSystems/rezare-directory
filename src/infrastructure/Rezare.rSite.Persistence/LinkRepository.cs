75aaa7694c89dfe97ab79c06cc7d1b9aaeef07fb
REZARE\LarkinsR <robert.larkins@rezare.co.nz>
2019-08-02 14:23:19 +1200

    Add more links to LinkRepository

--- src/infrastructure/Rezare.rSite.Persistence/LinkRepository.cs
+++ src/infrastructure/Rezare.rSite.Persistence/LinkRepository.cs
@@ -19,21 +19,52 @@ namespace Rezare.rSite.Persistence
         public IEnumerable<Link> GetLinks()
         {
 #pragma warning disable S1075
-            var path1 = "https://www.2talk.co.nz/login.html";
-            var path2 = "https://buildmaster.rezare.co.nz/";
-#pragma warning restore S1075
-
             var link1 = new Link(
-                new Uri(path1),
+                new Uri("https://www.2talk.co.nz/login.html"),
                 "2talk",
                 "The 2talk portal for managing your work phone.");
 
             var link2 = new Link(
-                new Uri(path2),
+                new Uri("https://buildmaster.rezare.co.nz/"),
                 "BuildMaster",
                 "Deployment system for Rezare projects.");
 
-            return new[] { link1, link2 };
+            var link3 = new Link(
+                new Uri("https://my.workflowmax.com"),
+                "WorkflowMax",
+                "Time recording.");
+
+            var link4 = new Link(
+                new Uri("https://breezy.hr/signin"),
+                "Breezy",
+                "Candidate Recruitment Platform.");
+
+            var link5 = new Link(
+                new Uri("https://secure.rezare.co.nz/gemini/"),
+                "Gemini",
+                "Story and issue tracking.");
+
+            var link6 = new Link(
+                new Uri("https://github.com/rezaresystems"),
+                "Rezare Github",
+                "Rezare's Github page for repositories.");
+
+            var link7 = new Link(
+                new Uri("coffee.rezare.co.nz"),
+                "Coffee Ordering",
+                "Hot drink selection for Rezare Staff meetings.");
+#pragma warning restore S1075
+
+            return new[]
+            {
+                link1,
+                link2,
+                link3,
+                link4,
+                link5,
+                link6,
+                link7
+            };
         }
     }
 }
